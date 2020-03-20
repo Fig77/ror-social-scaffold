81ec541027a62c3a2d45b84ad81dab81f0cc300f
Dandush03 <d.laloush@outlook.com>
2020-03-20 17:31:33 -0400

    Fixed Minor Bug

--- db/migrate/20200320210815_create_friend_requests.rb
+++ db/migrate/20200320210815_create_friend_requests.rb
@@ -1,14 +1,11 @@
 class CreateFriendRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :friend_requests do |t|
-      t.integer :creator
-      t.integer :friend
+      t.references :creator, references: :users, foreign_key: { to_table: :users}
+      t.references :friend, references: :users, foreign_key: { to_table: :users}
       t.integer :status
-      t.users :references
 
       t.timestamps
     end
-    add_foreign_key :friend_requests, :users, column: :creator
-    add_foreign_key :friend_requests, :users, column: :friend
   end
 end
