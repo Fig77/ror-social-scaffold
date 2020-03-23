1e8f69cb029006be99f52c5823e02f3814b9c0b3
Dandush03 <d.laloush@outlook.com>
2020-03-23 17:26:39 -0400

    added somes scss and fixed check status function

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -16,14 +16,4 @@ class FriendRequestsController < ApplicationController
   def friend_params
     params.require(:friend).permit(:status)
   end
-
-  def check_status(status, friend)
-    unless current_user.friend_requests.find_by_friend(friend).nil?
-      temp_status = current_user.friend_requests.find_by_status(status).status
-      case temp_status
-        when nil then 'pending'
-        when 0 then 'accepted'
-      else 'declined'
-      end
-    end
 end
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,6 @@
 class FriendRequest < ApplicationRecord
+  belongs_to :user
+  
   scope :pending, -> { where('status = ?', nil) } # nil = pending  
   scope :rejected, -> { where('status = ?', 0) }  # 0 = rejected
   scope :accepted, -> { where('status = ?', 1) }  # 1 = accepted
