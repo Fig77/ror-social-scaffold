43433472a1b9b7a5ba0875a1b9fd6848adaced48
Fig77 <fig.igle@gmail.com>
2020-03-24 14:22:50 -0300

    added update friend status functionality

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -5,11 +5,15 @@ class FriendRequestsController < ApplicationController
   end
 
   def destroy
-    # current_user.friend_requests.find(params[]).destroy
+    current_user.friend_requests.find_by(:friend_id => params[:id]).delete
     redirect_to request.referrer
   end
 
   def update
+    temp = User.find(params[:id])
+    temp = temp.friend_requests.find_by(:friend_id => current_user.id)
+    temp.status = params[:whatever]
+    temp.save
     redirect_to request.referrer
   end
 
