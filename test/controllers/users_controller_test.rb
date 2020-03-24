5a09933d9f459dfa214b9382607eb0378d3bb352
Fig77 <fig.igle@gmail.com>
2020-03-24 18:48:41 -0300

    Added test cases for user/1, added test case for users path

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  def current_user
+    @current_user
+  end
+
+  setup do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_url
+  end
+
+  test 'Will get all users from index' do
+    get users_path
+    assert_response :success
+  end
+
+  test 'Will show a specific  user' do
+  	get user_path(users(:user_002).id)
+    assert_response :success
+  end
+end
