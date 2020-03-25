3c4d37df735bfc5e4073d87cdb457dd98805d380
Fig77 <fig.igle@gmail.com>
2020-03-25 00:53:08 -0300

    deleted unused code

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -20,23 +20,3 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'div.post-comments', count: 1
   end
 end
-
-# class CommentsController < ApplicationController
-#   def create
-#     @comment = Comment.new(comment_params)
-#     @comment.post_id = params[:post_id]
-#     @comment.user = current_user
-
-#     if @comment.save
-#       redirect_to posts_path, notice: 'Comment was successfully created.'
-#     else
-#       redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
-#     end
-#   end
-
-#   private
-
-#   def comment_params
-#     params.require(:comment).permit(:content)
-#   end
-# end
