cc623bcce95902518b711ca62ababe9c5d94108b
Fig77 <fig.igle@gmail.com>
2020-03-25 21:50:51 -0300

    Friend gets created

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -5,26 +5,18 @@ class FriendRequestsController < ApplicationController
   end
 
   def destroy
-    current_user.pending_friendships.find_by(friend_id: params[:id]).delete
+    current_user.confirmed_friendships.find_by(friend_id: params[:id]).delete
     redirect_to request.referrer unless request.referrer.nil?
   end
 
   def update
-    temp = current_user.inverse_friendships.find_by(creator_id: params[:id])
-    temp.status = params[:whatever]
-    puts confirmed.inspect
+    temp = current_user.inverted_friendships.find_by(user_id: params[:id])
+    temp.confirm_friend
     temp.save
     redirect_to request.referrer unless request.referrer.nil?
   end
 
   private
-
-  def confirm_friend
-    update_attributes(confirmed: true)
-    Friendship.create!(friend_id: user_id,
-                       user_id: friend_id,
-                       confirmed: true)
-  end
 
   def friend_params
     params.require(:friend_request).permit(:friend)
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,12 @@
 class FriendRequest < ApplicationRecord
+
+  def confirm_friend
+    self.update_attributes(confirmed: true)
+    FriendRequest.create!(friend_id: user_id,
+                       user_id: friend_id,
+                       confirmed: true)
+  end
+
   belongs_to :user, class_name: 'User'
   belongs_to :friend, class_name: 'User'
 end
